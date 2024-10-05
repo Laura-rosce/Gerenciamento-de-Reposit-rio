2ff8ed179dbe4357ec0b6f2f3c38c70ff228adbe
Laura Roscelle <lauraroscelle2015@gmail.com>
2024-10-05 14:28:46 -0300

    2 Alteração: Mudar valores

--- python.py
+++ python.py
@@ -5,3 +5,18 @@ print ('==== Mini Calculadora ====\n')
 a = int(input('Informe um número: '))
 b = int(input('Informe outro número: '))
 resp = 0
+
+#2 alteração
+while resp == 0:
+    operacao = input ('Informe a operação (+,-,*,/): ')
+    if operacao == '+':
+        print(a+b)
+    elif operacao == '-':
+        print(a-b)
+    elif operacao == '*':
+        print (a*b)
+    elif operacao == '/':
+        print (a/b)
+    else:
+        print('Não foi possível identificar a operação!')
+    resp = int(input ('\nDesejas continuar? (sim = 0, não = 1): '))
